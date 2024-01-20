a9fe2578b951edb0d54c17e962e363db1f44298b
Andres <andrsbtrg@gmail.com>
2024-01-20 16:02:14 +0100

    Fix: Cache directory not working on Windows

--- shared/src/settings.rs
+++ shared/src/settings.rs
@@ -8,12 +8,12 @@ impl SettingsProvider {
         #[cfg(unix)]
         let app_data = std::env::var("HOME").expect("No HOME directory");
         #[cfg(windows)]
-        let app_data = std::env::var("APP_DATA").expect("No APP_DATA directory");
+        let app_data = std::env::var_os("APPDATA").expect("No APP_DATA directory");
         let path = std::path::Path::new(&app_data);
         let os = std::env::consts::OS;
         match os {
             "linux" => path.join(".var").join("app").join(Self::APP_NAME),
-            "windows" => path.join(&app_data),
+            "windows" => path.join(&app_data).join(Self::APP_NAME),
             _ => todo!("This os is not supported."),
         }
     }
